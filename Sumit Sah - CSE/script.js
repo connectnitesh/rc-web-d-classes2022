4db959382876f0b5b173328e3ac4f711eece0d72
connectnitesh <connectnitesh8@gmail.com>
2022-02-26 22:00:57 +0530

    final commit -4

---
+++ Sumit Sah - CSE/script.js
@@ -0,0 +1,9 @@
+function home(){
+  window.location.href = "home.html";
+}
+function about(){
+  window.location.href = "aboutus.html";
+}
+function contact(){
+  window.location.href = "contactus.html";
+}
